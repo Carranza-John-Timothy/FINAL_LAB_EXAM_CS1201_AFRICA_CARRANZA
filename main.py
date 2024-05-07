bb4b175e43df1f22662b2ff57518ccb7dd1b6593
Carranza-John-Timothy <157569639+Carranza-John-Timothy@users.noreply.github.com>
2024-05-07 09:57:55 +0800

    Final

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+
+import utils.user_manager
+import utils.dice_game
+import utils.score
+
 class Menu():
     def __init__(self): 
         pass
@@ -11,4 +16,12 @@ class Menu():
         choice = int(input("Please choose an option: "))
 
         if choice == 1:
-            #123123123123
+            utils.user_manager.register()
+        elif choice == 2:
+            utils.user_manager.login()
+        elif choice == 3:
+            return
+        
+if __name__ == "__main__":
+    Menu()
+
