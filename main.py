da00a806380b970e5c9e651fc3a6d2eeb5cda05f
Carranza-John-Timothy <23-05494@g.batstate-u.edu.ph>
2024-05-07 09:14:31 +0800

    update main

---
+++ main.py
@@ -0,0 +1,14 @@
+class Menu():
+    def __init__(self): 
+        pass
+
+    def menu(self):
+        print("Welcome to dice game!")
+        print("1. Register")
+        print("2. Login")
+        print("3. Exit")
+
+        choice = int(input("Please choose an option: "))
+
+        if choice == 1:
+            
